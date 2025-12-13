d3ae9f594b377a1d3900738eb6497eda453fbcfc
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2025-12-13 18:29:53 +0530

    cron added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
@@ -9,6 +10,7 @@ import {connectDB} from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
