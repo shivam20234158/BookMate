4d0bbc4b698f1dabcf3dd4a3915d5e8ceca228b0
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2025-12-14 17:32:53 +0530

    size increased

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 job.start();
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(cors());
 
 app.use("/api/auth",authRoutes);
